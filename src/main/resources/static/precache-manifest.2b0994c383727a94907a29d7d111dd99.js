3da96b5a45730d72a7367aec2e24c5ab4ae3b7e0
Seungwan Isaac Yuk <6winwan@gmail.com>
2019-09-16 16:33:33 -0600

    React added

---
+++ src/main/resources/static/precache-manifest.2b0994c383727a94907a29d7d111dd99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bba2322ab4c9aa19c6ad8e550611393",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10f8ec77e5472637a69c",
+    "url": "/static/css/2.df9ef6f4.chunk.css"
+  },
+  {
+    "revision": "6d10c06d970826edbec1",
+    "url": "/static/css/main.71a63cf9.chunk.css"
+  },
+  {
+    "revision": "10f8ec77e5472637a69c",
+    "url": "/static/js/2.9eb5555d.chunk.js"
+  },
+  {
+    "revision": "6d10c06d970826edbec1",
+    "url": "/static/js/main.5b47e4b7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
